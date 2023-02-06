98915dd8d9a8203532e51c1f77e4dfde9bdd0734
Red <mehulzinzuvadiya098@gmail.com>
2023-02-06 11:54:09 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,140 @@
+// project 1
+//
+// import 'package:flutter/material.dart';
+//
+// void main() {
+//   runApp(
+//     MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       home: Scaffold(
+//         body: Center(
+//           child: Container(
+//             height: 50,
+//             width: 300,
+//             alignment: Alignment.center,
+//             decoration: BoxDecoration(
+//               gradient: LinearGradient(colors: [
+//                 Colors.purple,
+//                 Colors.purpleAccent
+//               ]
+//               ),
+//               borderRadius: BorderRadius.circular(30)
+//             ),
+//
+//             child:Text("Flutter"
+//             ),
+//
+//           ),
+//         ),
+//       ),
+//     ),
+//   );
+// }
+
+// project 2
+
+// import 'package:flutter/material.dart';
+//
+// void main() {
+//   runApp(
+//     MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       home: Scaffold(
+//         body: Center(
+//           child: Container(
+//             height: 50,
+//             width: 250,
+//             alignment: Alignment.center,
+//             decoration: BoxDecoration(
+//               color: Colors.black87
+//             ),
+//             child:Text("🐦   Sing in with Twitter",style: TextStyle(
+//               fontSize: 20,
+//               color: Colors.white,
+//             ),
+//             ),
+//           ),
+//         ),
+//       ),
+//     ),
+//   );
+// }
+
+// project 3
+
+// import 'package:flutter/material.dart';
+//
+// void main() {
+//   runApp(
+//     MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       home: Scaffold(
+//         body: Center(
+//           child: Container(
+//             height: 50,
+//             width: 300,
+//             alignment: Alignment.centerLeft,
+//             decoration: BoxDecoration(
+//                 color: Colors.lightBlue,
+//                 borderRadius: BorderRadius.circular(30)),
+//             child: Container(
+//               height: 50,
+//               width: 50,
+//               alignment: Alignment.center,
+//               decoration: BoxDecoration(
+//                 color: Colors.lightBlueAccent,
+//                 shape: BoxShape.circle,
+//               ),
+//               child: Text(
+//                 ">",
+//                 style: TextStyle(
+//                   fontSize: 20,
+//                   color: Colors.white,
+//                 ),
+//               ),
+//             ),
+//           ),
+//         ),
+//       ),
+//     ),
+//   );
+// }
+
+// project 4
+
+// import 'package:flutter/material.dart';
+//
+// void main() {
+//   runApp(
+//     MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       home: Scaffold(
+//         body: Center(
+//           child: Container(
+//             height: 50,
+//             width: 300,
+//             alignment: Alignment.center,
+//             decoration: BoxDecoration(
+//                color: Colors.white,
+//                 borderRadius: BorderRadius.circular(10),
+//               boxShadow: [
+//                 BoxShadow(
+//                   color: Colors.purple.shade700,
+//                   blurRadius: 1,
+//                   spreadRadius: 3,
+//                   offset: Offset(0,0.7)
+//                 )
+//               ]
+//             ),
+//
+//             child:Text("Flutter",style: TextStyle(
+//               color: Colors.purple.shade700,
+//             ),
+//             ),
+//
+//           ),
+//         ),
+//       ),
+//     ),
+//   );
+// }
